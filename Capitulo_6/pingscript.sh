8560cde26c11fe7e30fb9480423d1bafde056374
Julio Ureña <juliourena33@gmail.com>
2020-05-26 08:12:53 -0400

    Update pingscript.sh

--- Capitulo_6/pingscript.sh
+++ Capitulo_6/pingscript.sh
@@ -1,12 +1,14 @@
-# Ejemplo tomado del Libro de Georgia Weidman
+# Ejemplo tomado del Libro de Georgia Weidman con algunas modificaciones.
 
-#!/bin/bash
+#/bin/bash
 if [ "$1" == "" ]
 then
-echo "Uso: ./pingscript.sh [direccion de red]"
-echo "Ejemplo: ./pingscript.sh 192.168.20"
+	echo "Uso: ./ping.sh [direccion de red]"
+	echo "Ejemplo: ./ping.sh 192.168.20"
 else
-for x in `seq 1 254`; do
-ping -c 1 $1.$x | grep "64 bytes" | cut -d" " -f4 | sed 's/.$//'
-done
+	echo "Direccion de red selecionada $1"
+	for ip in $(seq 1 254); do
+		# echo "Intento de Ping a la Direccion IP $ip"
+		ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f4 | tr -d ":" &
+	done
 fi
