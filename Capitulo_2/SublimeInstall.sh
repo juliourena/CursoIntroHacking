ee6b4972831f1f744351365920537d68c9c2eb4a
Julio Ureña <juliourena33@gmail.com>
2020-05-26 19:20:08 -0400

    Create SublimeInstall.sh

---
+++ Capitulo_2/SublimeInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Sublime Text
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
